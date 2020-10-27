b933007848a5c45a0fd9bd3105738492a11a4bbf
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-27 11:01:25 -0500

    [codealong] styling of media querys

---
+++ app.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container');
+
+container.addEventListener('mouseover', () => {
+    const newElement = createNewElement('p', 'This is super cool');
+    container.appendChild(newElement);
+});
+
+function createNewElement(tagName, text) {
+    // create a new element with tagName
+    const newElement = document.createElement(tagName);
+    // set the textContent to the new element  
+    newElement.textContent = text;  
+    // return the new element
+    return newElement;
+}
